06868ddea2142d87db0ad67713e0f0c86e73697a
manshikandu <manshikandu@gmail.com>
2025-08-30 12:36:12 +0545

    backend

--- backend/ChatApp/lib/socket.js
+++ backend/ChatApp/lib/socket.js
@@ -7,7 +7,11 @@ const app = express();
 const server = http.createServer(app);
 
 export const io = new Server(server, {
-  cors: { origin: ["http://localhost:5173"], methods: ["GET", "POST"] },
+  cors: { origin: ["http://localhost:5173",  // local development
+    "https://freelance-artist-booking-system-10.onrender.com"    //deployment ma rakhekoo
+  ], methods: ["GET", "POST"] ,
+  credentials: true,
+  },
 });
 
 const userSocketMap = new Map();
